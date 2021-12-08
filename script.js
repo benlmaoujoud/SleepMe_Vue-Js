5669dca80d92cd813b7e8ccd32ae2c994793b95d
Mohamed Benlmaoujoud <40149539+benlmaoujoud@users.noreply.github.com>
2021-12-08 20:51:45 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+new Vue({
+    el : '#app', //everything in app i can controle it 
+    data: {
+       hour:'',
+       min:'',
+       flag :'',
+       result : '',
+       selectedTime :'',
+       
+    },
+    methods: {
+       calcule : function(){
+    if(this.hour && this.min)
+    selectedTime = this.hour+":"+this.min+": 00"+" "+this.flag
+    var d = new Date(moment(selectedTime,'h:mm:ss a'))
+    
+    var time = moment.duration("08:00:00")
+    var date = moment(d)
+    date.subtract(time)
+    
+    this.result = "You should try to fall asleep at : "+moment(date).format("h:mm a")
+       
+    }
+    },
+
+    
+})
+
